9bd34797a945e200292521f131f4bbdd25ee6fe9
江島大輔 <eshimadaisuke@Daisuke-MacBook-Pro.local>
2019-10-26 20:00:42 +0900

    2019.10.26-2

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -12,7 +12,7 @@ class StatsController < ApplicationController
   def pacific_batter
   end
 
-  def personal_stats
+  def personal_pitcher_stats
     genre = ['防御率','WHIP','FIP','RSAA']
     aData = [10,10,7,6]
 
@@ -22,6 +22,23 @@ class StatsController < ApplicationController
       f.xAxis(categories: genre,tickmarkPlacement:'on')    
       f.yAxis(gridLineInterpolation: 'polygon',lineWidth:0,min:0,max:10) #各項目の最大値やら
       f.series(name:'山本由伸（オ）',data: aData,pointPlacement:'on')
+      f.legend(align: 'right',
+          verticalAlign: 'top',
+          y: 70,
+          layout: 'vertical')
+    end
+  end
+
+  def personal_batter_stats
+    genre = ['打率','ｗＯＢＡ','ｗＲＡＡ','ＲＣ２７']
+    aData = [9,9,7,6]
+
+    @graph = LazyHighCharts::HighChart.new('graph') do |f|
+      f.chart(polar: true,type:'line') #グラフの種類
+      f.pane(size:'90%')                  #グラフサイズの比
+      f.xAxis(categories: genre,tickmarkPlacement:'on')    
+      f.yAxis(gridLineInterpolation: 'polygon',lineWidth:0,min:0,max:10) #各項目の最大値やら
+      f.series(name:'森 友哉（西）',data: aData,pointPlacement:'on')
       f.legend(align: 'right',
           verticalAlign: 'top',
           y: 70,
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get '/c_batter', to:'stats#central_batter'
   get '/p_pitcher', to: 'stats#pacific_pitcher'
   get '/p_batter', to: 'stats#pacific_batter'
-  get '/personal_stats', to: 'stats#personal_stats'
+  get '/personal_pitcher_stats', to: 'stats#personal_pitcher_stats'
+  get '/personal_batter_stats', to: 'stats#personal_batter_stats'
+
 
 end
