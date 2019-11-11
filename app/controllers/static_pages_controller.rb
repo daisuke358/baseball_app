dc9d871d1886149921bd7e44420f19cf3d566dd3
daisuke358 <e.big358@gmail.com>
2019-11-11 23:11:13 +0900

    表示件数をcontrollerで指定

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,10 +4,10 @@ class StaticPagesController < ApplicationController
     @rank2 = 1
     @rank3 = 1
     @rank4 = 1
-    @pitchers_c = Pitcher.where(league: false).order('era')
-    @pitchers_p = Pitcher.where(league: true).order('era')   
-    @batters_c = Batter.where(league: false).order('avg DESC')
-    @batters_p = Batter.where(league: true).order('avg DESC')
+    @pitchers_c = Pitcher.where(league: false).order('era').first(5)
+    @pitchers_p = Pitcher.where(league: true).order('era').first(5)
+    @batters_c = Batter.where(league: false).order('avg DESC').first(5)
+    @batters_p = Batter.where(league: true).order('avg DESC').first(5)
   end
   def glossary
   end
